ec5dccb4cb0aaeb619f0f83b6f04b0fd81be9351
Maggie <mzhao3@stuy.edu>
2018-09-27 20:51:45 -0400

    changed bolding

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -23,8 +23,8 @@ def authenticate():
     ##  POST: requests that a web server accepts the data enclosed in the body of the request message, most likely for storing it.
     method = request.method
     greeting = "Hello <b>" + username + "</b>! I'm Rick Harrison, and this is my pawn shop. I work here with my old man and my son, Big Hoss. Everything in here has a story and a price. One thing I've learned after 21 years - you never know what is gonna come through that door."
-    
-    returnStr = "<b>Username: </b> " + username + "<br>" + "Method: <b> "+ method +"</b><br>"+greeting
+
+    returnStr = "Username: <b> " + username + "</b><br>" + "Method: <b> "+ method +"</b><br>"+greeting
 
     return returnStr
 
