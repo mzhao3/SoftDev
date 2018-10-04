7f972f97076e0f99d9b92ac41f09244daf91fd12
Maggie <mzhao3@stuy.edu>
2018-10-04 17:32:37 -0400

    added HW16- sqlite & python to create tables

---
+++ 16_cvs2db/db_builder.py
@@ -0,0 +1,56 @@
+# Maggie Zhao & Qian Zhou
+# SoftDev1 pd7
+# K16-- No Trouble
+# 2018-10-04
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitates CSV I/O
+
+#==========================================================
+with open('peeps.csv') as csvfile:
+    readerPeeps = csv.DictReader(csvfile)
+
+with open('courses.csv') as csvfile:
+    readerCourses = csv.DictReader(csvfile)
+
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
+
+#==========================================================
+#INSERT YOUR POPULATE CODE IN THIS ZONE
+
+
+#building peeps table
+
+command = "CREATE TABLE peeps(name TEXT, age INTEGER, id INTEGER)"
+
+#build SQL stmt, save as string
+c.execute(command)    #run SQL statement
+with open('peeps.csv') as csvfile:
+    readerPeeps = csv.DictReader(csvfile)
+    for row in readerPeeps:
+        command = "INSERT INTO peeps (name, age, id)VALUES (?,?,?)"
+        values = [(row["name"], row["age"], row["id"])]
+
+        c.executemany(command, values)
+
+
+
+command = "CREATE TABLE courses(name TEXT, age INTEGER, id INTEGER)"
+
+#build SQL stmt, save as string
+c.execute(command)
+
+with open('courses.csv') as csvfile:
+    readerCourses = csv.DictReader(csvfile)
+    for row in readerCourses:
+        command = "INSERT INTO courses (name, age, id)VALUES (?,?,?)"
+        values = [(row["code"], row["mark"], row["id"])]
+
+        c.executemany(command, values)
+#==========================================================
+
+db.commit() #save changes
+db.close()  #close database
