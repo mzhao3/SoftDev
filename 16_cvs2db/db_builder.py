3217d56347c8e97b542a92fd5939be357035e71a
Maggie <mzhao3@stuy.edu>
2018-10-04 21:10:47 -0400

    added comments

--- 16_cvs2db/db_builder.py
+++ 16_cvs2db/db_builder.py
@@ -1,4 +1,4 @@
-# Maggie Zhao & Qian Zhou
+# Small Elephants: Maggie Zhao & Qian Zhou
 # SoftDev1 pd7
 # K16-- No Trouble
 # 2018-10-04
@@ -7,11 +7,6 @@ import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
 
 #==========================================================
-with open('peeps.csv') as csvfile:
-    readerPeeps = csv.DictReader(csvfile)
-
-with open('courses.csv') as csvfile:
-    readerCourses = csv.DictReader(csvfile)
 
 DB_FILE="discobandit.db"
 
@@ -23,7 +18,6 @@ c = db.cursor()               #facilitate db ops
 
 
 #building peeps table
-
 command = "CREATE TABLE peeps(name TEXT, age INTEGER, id INTEGER)"
 
 #build SQL stmt, save as string
@@ -31,13 +25,17 @@ c.execute(command)    #run SQL statement
 with open('peeps.csv') as csvfile:
     readerPeeps = csv.DictReader(csvfile)
     for row in readerPeeps:
+        # '?' is used as a placeholder for values. provide a tuple of values as the second argument to the execute() method
+        # from the sqlite documentation: You shouldn't assemble your query using Python's string operations because doing so is insecure; it makes your program vulnerable to an SQL injection attack
         command = "INSERT INTO peeps (name, age, id)VALUES (?,?,?)"
         values = [(row["name"], row["age"], row["id"])]
 
+        # executemany(SQL, [parameters]) where parameters is a list of tuples
         c.executemany(command, values)
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
+#building courses table
 command = "CREATE TABLE courses(name TEXT, age INTEGER, id INTEGER)"
 
 #build SQL stmt, save as string
@@ -50,6 +48,8 @@ with open('courses.csv') as csvfile:
         values = [(row["code"], row["mark"], row["id"])]
 
         c.executemany(command, values)
+
+
 #==========================================================
 
 db.commit() #save changes
